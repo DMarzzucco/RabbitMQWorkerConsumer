3fd36661b399b456a352f203ab5987e53ff967b0
Dmarzzucco <ksksks>
2024-05-16 15:01:13 -0300

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/', (_req, res) => {
+    console.log("conectado")
+    res.send("enviado")
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
