4452df084fbc9e1209c52fce5f44ab4587afd96e
Dmarzzucco <ksksks>
2024-05-21 14:32:25 -0300

    ksks

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,7 +1,15 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 
 function Home() {
     const [valueIm, setValueIm] = useState<number>(0)
+    const [menu, setMenu] = useState<boolean>(false)
+    const [auth, setAuth] = useState<string | number>();
+
+    const TakeAuth = (event: ChangeEvent<HTMLInputElement>) => {
+        setAuth(event.target.value);
+    }
+
+    const showMenu = () => { setMenu(!menu) }
     const plus = ({ }) => {
         setValueIm(preValue => preValue + 1)
     }
@@ -10,6 +18,9 @@ function Home() {
         if (valueIm > 0) {
             setValueIm(prevValue => prevValue - 1)
         }
+    }
+    const Authenticator = ({ }) => {
+        // if (setAuth === 2) { }
     }
     return (
         <>
@@ -22,6 +33,18 @@ function Home() {
                     <button className="m-2 p-2 border  text-xl" onClick={rest}>-</button>
                     <input className="w-20 h-20 text-center" type="text" value={valueIm} readOnly />
                     <button className="m-2 p-2 border  text-xl" onClick={plus}>+</button>
+                </div>
+
+                <div className="flex flex-col justify-center items-center">
+                    <span>Para entrar a la pagina darle aqui</span>
+                    <button onClick={showMenu}>INGRESAR</button>
+                    {menu ?
+                        <div>
+                            <input type="text" placeholder="contraseña" value={auth} onChange={TakeAuth} />
+                            <button></button>
+
+                        </div>
+                        : null}
                 </div>
             </section>
         </>
